f9f63509b1facc47b69d83a91dede451b593d2b2
Kexin <kkc2160@columbia.edu>
2025-07-07 03:31:13 +0100

    Added game verifier

--- script.js
+++ script.js
@@ -24,16 +24,12 @@ boxes.forEach(box => {
         }
         box.classList.add("played")
         turnCount++;
-        check = isGameOver([box, input]);
+        check = isGameOver();
     });
 });
 
-// function for if game is over
-function isGameOver(last_move) {
-    /* Calculates if the tic-tac-toe game is over based on the current board and last move
-    *
-    * @param 
-    * 
+function isGameOver() {
+    /* Calculates if the tic-tac-toe game is over based on the current board 
     */
     let x_boxes = document.querySelectorAll(".X");
     let o_boxes = document.querySelectorAll(".O");
@@ -41,6 +37,58 @@ function isGameOver(last_move) {
         return false;
     }
 
+    // all horizontal case 
+    for (let i = 0; i < 3; i++) {
+        let box0 = document.getElementById(`row-${i}-box-0`);
+        let box1 = document.getElementById(`row-${i}-box-1`);
+        let box2 = document.getElementById(`row-${i}-box-2`);
+
+        if (box0 in x_boxes && box1 in x_boxes && box2 in x_boxes) {
+            return true
+        }
+        if (box0 in o_boxes && box1 in o_boxes && box2 in o_boxes) {
+            return true
+        }
+    }
+
+    // all vertical case 
+    for (let i = 0; i < 3; i++) {
+        let box0 = document.getElementById(`row-0-box-${i}`);
+        let box1 = document.getElementById(`row-1-box-${i}`);
+        let box2 = document.getElementById(`row-2-box-${i}`);
+
+        if (box0 in x_boxes && box1 in x_boxes && box2 in x_boxes) {
+            return true
+        }
+        if (box0 in o_boxes && box1 in o_boxes && box2 in o_boxes) {
+            return true
+        }
+    }
+
+    // diagonals 
+    let box0 = document.getElementById(`row-0-box-0`);
+    let box1 = document.getElementById(`row-1-box-1`);
+    let box2 = document.getElementById(`row-2-box-2`);
+
+    if (box0 in x_boxes && box1 in x_boxes && box2 in x_boxes) {
+        return true
+    }
+    if (box0 in o_boxes && box1 in o_boxes && box2 in o_boxes) {
+        return true
+    }
+
+    box0 = document.getElementById(`row-0-box-2`);
+    box1 = document.getElementById(`row-1-box-1`);
+    box2 = document.getElementById(`row-2-box-0`);
+
+    if (box0 in x_boxes && box1 in x_boxes && box2 in x_boxes) {
+        return true
+    }
+    if (box0 in o_boxes && box1 in o_boxes && box2 in o_boxes) {
+        return true
+    }
+
+    return false
 }
 
 // function for get AI guess
