04c751de6c21a35c8796c08b08440bdf239d612d
Kexin <kkc2160@columbia.edu>
2025-08-03 07:51:37 +0100

    Implemented min max algorithm

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let AiToggle = document.getElementById("ai-select")
 let vsAI = 1; // starts 
 const PLAYER_X = 1;
 const PLAYER_O = 2;
+const WIN_SCORE = 10;
+const LOSE_SCORE = -10;
 
 // let board = {
 //     row0box0: "-",
@@ -29,53 +31,75 @@ boxes.forEach((box, index) => {
         row = Math.floor(index / 3);
         column = index % 3;
         console.log("Clicked: ", box);
-        if (check == PLAYER_X || check == PLAYER_O) { // game already over 
+        if (check === PLAYER_X || check === PLAYER_O) { // game already over 
             return;
         }
         if (box.classList.contains("played")) {
             return;
         }
-        if (turnCount % 2 == 0) {
-            box.textContent = "X";
-            box.classList.add("X");
-            // board[`row${row}box${column}`] = "X";
-            board_arr[index] = "X";
-            console.log(`board_arr[${index}] = "X"`);
-        }
-        else {
-            box.textContent = "O";
-            box.classList.add("O");
-            // board[`row${row}box${column}`] = "O";
-            // console.log(board[`row${row}box${column}`]);
-            board_arr[index] = "O";
-            console.log(`board_arr[${index}] = "O"`);
-        }
-        box.classList.add("played");
-        turnCount++;
-        check = isGameOver(); // returns 0 when game not over, 1 for X win, 2 for O win
+        updateBoard(box, index);
+
+        check = isGameOver(board_arr); // returns 0 when game not over, 1 for X win, 2 for O win, 3 for draw
         console.log("Check: ", check)
+
+        if (check === 0 && vsAI === 1) {
+            let ai_guess_index = get_best_guess();
+            let ai_box = document.getElementById(index);
+            updateBoard(ai_box, ai_guess_index);
+        }
     });
 });
 
-function isGameOver() {
+function updateBoard(box, index) {
+    if (turnCount % 2 == 0) {
+        box.textContent = "X";
+        box.classList.add("X");
+        // board[`row${row}box${column}`] = "X";
+        board_arr[index] = "X";
+        console.log(`board_arr[${index}] = "X"`);
+    }
+    else {
+        box.textContent = "O";
+        box.classList.add("O");
+        // board[`row${row}box${column}`] = "O";
+        // console.log(board[`row${row}box${column}`]);
+        board_arr[index] = "O";
+        console.log(`board_arr[${index}] = "O"`);
+    }
+    box.classList.add("played");
+    turnCount++;
+}
+
+function isGameOver(board) {
     /* Calculates if the tic-tac-toe game is over based on the current board 
     */
-
-    let x_boxes = Array.from(document.querySelectorAll(".X"));
-    let o_boxes = Array.from(document.querySelectorAll(".O"));
+    let x_boxes = [];
+    let o_boxes = [];
+    let none_boxes_count = 0;
+    for (let i = 0; i < board.length; i++) {
+        if (board[i] === "X") {
+            x_boxes.push(i);
+        }
+        else if (board[i] === "O") {
+            o_boxes.push(i);
+        }
+        else {
+            none_boxes_count++;
+        }
+    }
 
     if (x_boxes.length < 3 && o_boxes.length < 3) {
         console.log(x_boxes);
         console.log(o_boxes);
-        console.log("Not enough moves completed.");
+        console.log("Game not over: not enough moves completed.");
         return 0;
     }
 
     // all horizontal case 
     for (let i = 0; i < 3; i++) {
-        let box0 = document.getElementById(`row-${i}-box-0`);
-        let box1 = document.getElementById(`row-${i}-box-1`);
-        let box2 = document.getElementById(`row-${i}-box-2`);
+        let box0 = i * 3 + 0;
+        let box1 = i * 3 + 1;
+        let box2 = i * 3 + 2;
 
         if (x_boxes.includes(box0) && x_boxes.includes(box1) && x_boxes.includes(box2)) {
             return PLAYER_X;
@@ -87,9 +111,9 @@ function isGameOver() {
 
     // all vertical case 
     for (let i = 0; i < 3; i++) {
-        let box0 = document.getElementById(`row-0-box-${i}`);
-        let box1 = document.getElementById(`row-1-box-${i}`);
-        let box2 = document.getElementById(`row-2-box-${i}`);
+        let box0 = 0 + i;
+        let box1 = 3 + i;
+        let box2 = 6 + i;
 
         if (x_boxes.includes(box0) && x_boxes.includes(box1) && x_boxes.includes(box2)) {
             return PLAYER_X;
@@ -100,9 +124,9 @@ function isGameOver() {
     }
 
     // diagonals 
-    let box0 = document.getElementById(`row-0-box-0`);
-    let box1 = document.getElementById(`row-1-box-1`);
-    let box2 = document.getElementById(`row-2-box-2`);
+    let box0 = 0;
+    let box1 = 4;
+    let box2 = 8;
 
     if (x_boxes.includes(box0) && x_boxes.includes(box1) && x_boxes.includes(box2)) {
         return PLAYER_X;
@@ -111,15 +135,19 @@ function isGameOver() {
         return PLAYER_O;
     }
 
-    box0 = document.getElementById(`row-0-box-2`);
-    box1 = document.getElementById(`row-1-box-1`);
-    box2 = document.getElementById(`row-2-box-0`);
+    box0 = 2;
+    box1 = 4;
+    box2 = 6;
 
     if (x_boxes.includes(box0) && x_boxes.includes(box1) && x_boxes.includes(box2)) {
         return PLAYER_X;
     }
     if (o_boxes.includes(box0) && o_boxes.includes(box1) && o_boxes.includes(box2)) {
         return PLAYER_O;
+    }
+
+    if (none_boxes_count === 0) {
+        return 3;
     }
     return 0;
 }
@@ -130,39 +158,37 @@ function getAIGuess() {
         return [1, 1];
     }
     else {
-        let guess = get_best_guess();
+        return get_best_guess();
     }
-    return guess;
 }
 
 function get_best_guess() {
     let moves = [];
     let max_player = 0;
-    let min_player = 0;
     let best_mini_max = Number.NEGATIVE_INFINITY;
     let best_move = -1;
 
     // compute players 
     if (turnCount % 2 == 0) {
         max_player = PLAYER_X;
-        min_player = PLAYER_O;
     }
     else {
         max_player = PLAYER_O;
-        min_player = PLAYER_X;
     }
 
     // populate moves
     for (let i = 0; i < board_arr.length; i++) {
         if (board_arr[i] === "-") {
-            moves.append(i);
+            moves.push(i);
         }
     }
 
     // get minmax value for each move 
+    let isMax = true; // you always want to optimize for the player calling the function
     for (let i = 0; i < moves.length; i++) {
-        let board_copy = [...board];
-        let value = minimax(board_copy, moves[i], max_player, min_player, Number.NEGATIVE_INFINITY, 0);
+        let board_copy = [...board_arr];
+        let moves_copy = [...moves];
+        let value = minimax(board_copy, max_player, moves[i], isMax, turnCount, moves_copy, moves_made);
         if (value > best_mini_max) {
             best_mini_max = value;
             best_move = moves[i];
@@ -171,9 +197,70 @@ function get_best_guess() {
     return best_move;
 }
 
-function minimax(board, index, max_player, min_player, value, more_move_count) {
+function minimax(board, index, max_player, isMax, turn_count, moves, moves_made) {
+    // make the current move
+    const turn = turn_count % 2;
+    if (turn === PLAYER_X) {
+        board[index] = "X";
+    }
+    else {
+        board[index] = "O";
+    }
 
-    return value;
+    // remove index (move just made) from moves array
+    const moves_index = moves.indexOf(index);
+    if (moves_index === -1) {
+        console.log("Error: index not found in moves array");
+    }
+    else {
+        moves.splice(moves_index, 1);
+    }
+
+    // check if game over 
+    let game_result = isGameOver(board);
+    if (moves.length === 0 || game_result != 0) {
+        // needs more implementation, does value change here or later? 
+        // probably here
+        // every minimax call runs to this base case, 
+        // so yeah this is the only one that can do value assignment 
+        if (game_result === max_player) {
+            return WIN_SCORE - moves_made;
+        }
+        else if (game_result === 3) {
+            return 0;
+        }
+        else {
+            return LOSE_SCORE + moves_made;
+        }
+    }
+
+    // run more minimaxs for all possible moves 
+    let best_mini_max = 0
+    if (isMax) {
+        best_mini_max = Number.NEGATIVE_INFINITY;
+    }
+    else {
+        best_mini_max = Number.POSITIVE_INFINITY;
+    }
+    turn_count++;
+
+    if (isMax) {
+        for (let i = 0; i < moves.length; i++) {
+            let moves_copy = [...moves];
+            let board_copy = [...board];
+            let value = minimax(board_copy, max_player, moves[i], false, turn_count, moves_copy, moves_made + 1);
+            best_mini_max = Math.max(best_mini_max, value);
+        }
+    }
+    else {
+        for (let i = 0; i < moves.length; i++) {
+            let moves_copy = [...moves];
+            let board_copy = [...board];
+            let value = minimax(board_copy, max_player, moves[i], true, turn_count, moves_copy, moves_made + 1);
+            best_mini_max = Math.min(best_mini_max, value);
+        }
+    }
+    return best_mini_max;
 }
 
 AiToggle.addEventListener("input", () => {
